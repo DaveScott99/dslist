1e6cee9041b410caafb22706259cb6e9445f734d
Davi Santos <davicalixto2077@gmail.com>
2023-05-09 19:23:35 -0300

    Game Seed

--- src/main/java/com/astro/dslist/entities/Game.java
+++ src/main/java/com/astro/dslist/entities/Game.java
@@ -25,14 +25,19 @@ public class Game implements Serializable {
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
@@ -40,6 +45,7 @@ public class Game implements Serializable {
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -83,6 +89,14 @@ public class Game implements Serializable {
 
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
