4f36e8e8332adf224844127a53ca61a0c7aa4ddc
Davi Santos <davicalixto2077@gmail.com>
2023-05-09 18:52:13 -0300

    Project created

---
+++ src/test/java/com/astro/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
