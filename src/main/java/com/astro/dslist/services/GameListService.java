cef94955e230f0dfd849e0fd5e323fdf814161c4
Davi Santos <davicalixto2077@gmail.com>
2023-05-12 12:17:51 -0300

    Move games on list

--- src/main/java/com/astro/dslist/services/GameListService.java
+++ src/main/java/com/astro/dslist/services/GameListService.java
@@ -8,13 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.astro.dslist.dto.GameListDTO;
 import com.astro.dslist.entities.GameList;
+import com.astro.dslist.projections.GameMinProjection;
 import com.astro.dslist.repositories.GameListRepository;
+import com.astro.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
@@ -24,5 +29,20 @@ public class GameListService {
 					.toList();
 	}
 	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		Integer min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		Integer max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
 	
 }
